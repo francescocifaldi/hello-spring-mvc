54cb21027d87085e87b66f2f09f2056793363b8a
francescocifaldi <francesco.cifaldi@gmail.com>
2025-05-20 10:17:49 +0200

    ADD routing

--- src/main/java/org/lessons/java/hello_spring_mvc/controller/HomeController.java
+++ src/main/java/org/lessons/java/hello_spring_mvc/controller/HomeController.java
@@ -2,9 +2,24 @@ package org.lessons.java.hello_spring_mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class HomeController {
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("test")
+    public String test() {
+        return "test";
+    }
+
+    @GetMapping("prova")
+    public String prova() {
+        return "prova";
+    }
+
 }
